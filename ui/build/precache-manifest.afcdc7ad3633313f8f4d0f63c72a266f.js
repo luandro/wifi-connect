afc827678684703ed640534a526955bac5cf0a03
luandro <luandro@gmail.com>
2022-02-20 21:15:30 -0300

    Add modified UI build

---
+++ ui/build/precache-manifest.afcdc7ad3633313f8f4d0f63c72a266f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c69a9980d4082304af27a4ba94a9963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a318c505e40f03466edc",
+    "url": "/static/js/2.8d2ca71a.chunk.js"
+  },
+  {
+    "revision": "928d49cbbf6efabc30d168045df5b370",
+    "url": "/static/js/2.8d2ca71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed3974e1fb7f2631c6d",
+    "url": "/static/js/main.2fc13441.chunk.js"
+  },
+  {
+    "revision": "457afbf6f094802ddf65",
+    "url": "/static/js/runtime-main.299df88b.js"
+  },
+  {
+    "revision": "34c0c94e712ddb861346d68dfb00ab87",
+    "url": "/static/media/logo.34c0c94e.svg"
+  }
+]);
